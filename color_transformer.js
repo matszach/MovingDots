209779c21623bb1e93ef844bb0f005ce0692f4d0
Lukasz <lkaszubowski@gmail.com>
2019-03-19 14:01:57 +0100

    dots animations are now independent of each other

--- color_transformer.js
+++ color_transformer.js
@@ -1,6 +1,13 @@
-var isColor1 = true;
 
-function colorShiftAnimation(elementId,color1,color2,speed){
+// wrapper function, allows individual 'isColor1' var for each element
+function runColorAnimation(elementId,color1,color2,speed){
+    var isColor1 = true;
+    colorShiftAnimation(elementId,color1,color2,speed,isColor1);
+}
+
+// function that performs the actualt color shift, and then loops afetr a timeout.
+// Transition time set in .css file
+function colorShiftAnimation(elementId,color1,color2,speed,isColor1){
     var elem = document.getElementById(elementId);
     if(isColor1){
         elem.style.backgroundColor = color1;
@@ -10,6 +17,8 @@ function colorShiftAnimation(elementId,color1,color2,speed){
         isColor1 = true;
     }
     //passes function as a string, and the timeout length, resulting in ongoing loop
-    var functionAsString = `colorShiftAnimation(\'${elementId}\',\'${color1}\',\'${color2}\',${speed})`
+    var functionAsString = `colorShiftAnimation(\'${elementId}\',\'${color1}\',\'${color2}\',${speed},${isColor1})`
     setTimeout(functionAsString,speed);
 }
+
+
